9f7506a693c5f559492d831f130ab0fb89b4bd63
kwonhyukil <gurdlf320@naver.com>
2024-11-12 22:11:16 +0900

    Special Class_Java

---
+++ src/special_Class/_11_12/_04_Problem.java
@@ -0,0 +1,19 @@
+package special_Class._11_12;
+
+public class _04_Problem {
+    public static void main(String[] args) {
+//
+//      입력 값 : Row, 홀수만 입력 가능, 짝수 입력 시 재입력
+//
+//            0   1   2
+//
+//      Row 0   0   -   -
+//         1   0   0   -
+//         2   0   0   0
+//         3   0   0   -
+//         4   0   -   -
+
+
+    }
+
+}
