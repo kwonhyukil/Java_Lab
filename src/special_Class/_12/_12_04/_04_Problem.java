a6e69b6d5495c4503083cc1074ece5ee4a2af6af
kwonhyukil <gurdlf320@naver.com>
2024-12-06 21:46:02 +0900

    Special Class_Java

---
+++ src/special_Class/_12/_12_04/_04_Problem.java
@@ -0,0 +1,16 @@
+package special_Class._12._12_04;
+
+public class _04_Problem {
+    public static void main(String[] args) {
+        int[][] numbers = {
+                {20,30,40},
+                {50,60,70}
+        };
+        for (int[] value : numbers) {
+            System.out.println(value[0] + value[1] + value[2]);
+        }
+    }
+
+
+
+}
